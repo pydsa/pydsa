3afad42a4880d7ce217f3d232c7ae662cb6a56d1
AMiT Kumar <dtu.amit@gmail.com>
2015-12-28 23:04:24 +0530

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pydsa',
+      version='0.1a1',
+      description="Python Data Structure and Algorithms Library",
+      long_description="",
+      author='Amit Kumar',
+      author_email='dtu.amit@gmail.com',
+      license='MIT',
+      packages=['pydsa'],
+      zip_safe=False,
+      )
