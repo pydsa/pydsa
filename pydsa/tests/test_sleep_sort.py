fc3d63fc1aafe5b72574c0021b70ad3a56400100
Sumith1896 <sumith1896@gmail.com>
2016-03-02 19:20:18 +0530

    Add tests for sleep sort

--- pydsa/tests/test_sleep_sort.py
+++ pydsa/tests/test_sleep_sort.py
@@ -3,5 +3,5 @@ from random import randint
 
 
 def test_sleep_sort():
-    a = b = [randint(1, 100) for i in range(100)]
+    a = b = [randint(1, 3) for i in range(3)]
     assert sleep_sort(a) == sorted(b)
