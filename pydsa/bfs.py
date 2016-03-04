949525e37259a183f336782d14bfabb6fcfb0318
AMiT Kumar <dtu.amit@gmail.com>
2016-03-04 11:07:40 +0530

    Clean up code and fix PEP8

--- pydsa/bfs.py
+++ pydsa/bfs.py
@@ -1,44 +1,17 @@
 # Breadth First Search
 # Complexity: O(V+E)
 
-def bfs(graph,start):
 
-	"""
-	Traverses the graph 'graph' using Breadth First
-	Search Algorithm. 
-
-	>>> from pydsa import bfs
-	>>> graph = {'A': set(['B', 'C']), 'B': set(['A', 'D', 'E']), 'C': set(['A', 'F']), 'D': set(['B']), 'E': set(['B', 'F']), 'F': set(['C', 'E'])}
-	>>> bfs(graph,'A')
-	['A', 'C', 'B', 'F', 'E', 'D']
-
-	"""
-
-	# mark every node unvisited
-	visited , queue = {}, []
-	for i in graph.keys():
-		visited[i] = 0
-
-	# final list of traversed nodes in the order
-	final = []
-
-	# mark given node visited and enqueue
-	visited[start] = 1
-	queue.append(start)
-
-	while queue:
-
-		# dequeue a node and append in the final list
-		start = queue.pop(0)
-		final.append(start)
-
-		# check for all adjacent vertices of start vertex
-		# if it is not visited, then mark and 
-		# enqueue
-		for i in graph[start]:
-			if not visited[i]:
-				visited[i] = 1
-				queue.append(i)
-
-	# return the final ordered list
-	return final
+def bfs(graph, start):
+    """
+    Traverses a graph using Breadth First Search Algorithm.
+    It returns a set of nodes traversed.
+    """
+    visited, queue = set(), [start]
+    while queue:
+        node = queue.pop(0)
+        if node not in visited:
+            visited.add(node)
+            # Add all the adjacent unvisited nodes to the queue
+            queue.extend(graph[node] - visited)
+    return visited
