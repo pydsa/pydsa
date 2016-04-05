b2f9e9c1f55596ad4741f852356c1b8acfb7da50
soummyaah <soumya13106@iiitd.ac.in>
2016-04-05 18:22:08 +0530

    Merged tests for Binary Search Recursive and Non-Recursive

--- pydsa/tests/test_binary_search.py
+++ pydsa/tests/test_binary_search.py
@@ -1,12 +1,19 @@
 from pydsa.binary_search import binary_search
 from random import randint
 
+
 def test_binary_search():
     a = [randint(1, 10) for i in range(10)]
     item = randint(1, 10)
     a = sorted(a)
     ans = binary_search(a, item)
-    if(ans == -1): 
+    if(ans == -1):
         assert item not in a
-    else: 
+    else:
         assert a[ans] == item
+    ans1 = binary_search(a, item, True)
+    ans2 = binary_search(a, item, True, 0, len(a))
+    if(ans1 == ans2 == -1):
+        assert item not in a
+    else:
+        assert a[ans1] == item == a[ans2]
