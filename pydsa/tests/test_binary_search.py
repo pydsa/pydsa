880180fb3c81d503f8b59d1cad7d51cdfb231c78
soummyaah <soumya13106@iiitd.ac.in>
2016-04-08 23:28:39 +0530

    Broke functionality into two different functions

--- pydsa/tests/test_binary_search.py
+++ pydsa/tests/test_binary_search.py
@@ -17,3 +17,14 @@ def test_binary_search():
         assert item not in a
     else:
         assert a[ans1] == item == a[ans2]
+
+def test_binary_search_recursive():
+    a = [randint(1, 10) for i in range(10)]
+    item = randint(1, 10)
+    a = sorted(a)
+    ans_through_method_1 = binary_search(a, item, True)
+    ans_through_method_2 = binary_search(a, item, True, 0, len(a))
+    if(ans_through_method_1 == ans_through_method_2 == -1):
+        assert item not in a
+    else:
+        assert a[ans_through_method_1] == item == a[ans_through_method_2]
